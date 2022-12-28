e6a44fca8bc9666522dc066ff45d63bbf30dabf7
Dawid Segieta <buler43221@gmail.com>
2022-12-28 19:48:37 +0100

    toDoList2

--- js/script.js
+++ js/script.js
@@ -1,19 +1,49 @@
+/*
 const input = document.getElementById('input');
 const addInput = document.getElementById('add-input');
-const todos = document.getElementById('todos');
+const list = document.getElementById('list');
 
 addInput.addEventListener('click', function(){
-  let todo = document.createElement('p');
-  todo.className = 'todo';
-  todo.innerText = input.value;
-  todos.appendChild(todo);
+  let task = document.createElement('p');
+  task.className = 'task';
+  task.innerText = input.value;
+  list.appendChild(task);
   input.value = '';
 
-  todo.addEventListener('click', function(){
-    todo.style.textDecoration = 'line-through';
+  task.addEventListener('click', function(){
+    task.style.textDecoration = 'line-through';
   });
 
-  todo.addEventListener('dblclick', function() {
-    todos.removeChild(todo);   
+  task.addEventListener('dblclick', function() {
+    list.removeChild(task);   
   });
 });
+*/
+
+
+
+const input = document.getElementById('input');
+const addInput = document.getElementById('add-input');
+const list = document.getElementById('list');
+//const task = document.getElementById('task');
+const done = document.getElementById('done');
+const clear = document.getElementById('clear');
+
+addInput.addEventListener('click', function(){
+  let task = document.createElement('li', 'done');
+  task.classname = 'task';
+  task.innerText = input.value;
+  list.appendChild('task');
+  input.value = '';
+
+  done.addEventListener('click', function(){
+    task.style.textDecoration = 'line-through';
+  });
+
+  clear.addEventListener('click', function(){
+    //const task = document.querySelectorAll('li');
+    list.removeChild('task');
+  });
+});
+
+
